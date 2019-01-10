13b63dd0141b8e53c6e2e8c38e0d9a6d89d7bb85
Maciej Krüger <mkg20001@gmail.com>
2019-01-10 20:24:40 +0100

    feat: add bin.js

--- src/server/bin.js
+++ src/server/bin.js
@@ -1,8 +1,28 @@
 'use strict'
 
+// Usage: $0 [<address> <address 2>...]
+
+/* eslint-disable no-console */
+
 const Server = require('.')
 
-const server = new Server({})
+let addresses = process.argv.slice(2)
+if (!addresses.length) { addresses = null } // use default if none provided
+
+const server = new Server({ addresses })
 server.start().then(() => {
-  console.log('Started!')
-})
+  server.switch.addresses.map(String).forEach(addr => console.log('Listening on %s', addr))
+}, err)
+
+function stop () {
+  console.log('Stopping...')
+  server.stop().then(() => process.exit(0), err)
+}
+
+function err (err) {
+  console.error(err.stack)
+  process.exit(2)
+}
+
+process.on('SIGTERM', stop)
+process.on('SIGINT', stop)
